ca3df4019f262ea73c4dc0a9eab056a2319147aa
Aroldo <aroldo@digital.gob.cl>
2019-07-23 17:47:40 -0400

    pruebas básicas de operaciones matemáticas

---
+++ Tests/maths.test.js
@@ -0,0 +1,19 @@
+import Maths from '../maths'
+
+describe('Calculos matematicos', () => {
+    test('Prueba de suma', () => {
+        expect(Maths.sumar(1,1)).toBe(2);
+    });
+
+    test('Prueva de resta', () => {
+        expect(Maths.restar(3,2)).toBe(1);
+    });
+
+    test('Prueba de multiplicación', () => {
+        expect(Maths.multiplicar(3,2)).toBe(6);
+    });
+
+    test('Prueba de división', () => {
+        expect(Maths.dividir(100, 5)).toBe(20);
+    });
+});
---
+++ maths.js
@@ -0,0 +1,22 @@
+const sumar = (a, b) => {
+    return a + b;
+};
+
+const multiplicar = (a, b) => {
+    return a * b;
+};
+
+const restar = (a, b) => {
+    return a - b;
+};
+
+const dividir = (a, b) => {
+    return a / b;
+};
+
+export default {
+    sumar,
+    multiplicar,
+    restar,
+    dividir
+}
